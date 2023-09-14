9c37068d68a956de98d108e4bba8d8f1a47a8ac4
dhananjay370 <125184802+dhananjay370@users.noreply.github.com>
2023-09-14 16:25:24 +0000

    array problem

--- First.java
+++ First.java
@@ -1,5 +1,16 @@
 public class First{
     public static void main(String args[]){
         System.out.println("Hello World");
+        int arr[] = {5,8,3,4,9,2};
+        int target = 6;
+        int n = arr.length;
+        for(int i = 0; i < n - 1; i++){
+            for(int j = i + 1; j < n; j++){
+                if((arr[i]+arr[j])==target){
+                    System.out.println(i+" "+j);
+                    break;
+                }
+            }
+        }
     }
 }
