e426b0adc76565e9cd8357bde23ee3423422d30f
dhananjay370 <125184802+dhananjay370@users.noreply.github.com>
2023-09-15 14:00:44 +0000

    NumPattern

---
+++ NumPattern.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = 4;
+    int row = 1;
+    while (row <= n)
+    {
+        int space = n - row;
+        while (space)
+        {
+            cout << " "
+                 << " ";
+            space--;
+        }
+        int j = 1;
+        while (j <= row)
+        {
+            cout << j << " ";
+            j++;
+        }
+
+        int start = row - 1;
+        while (start)
+        {
+            cout << start << " ";
+            start--;
+        }
+        cout << endl;
+        row++;
+    }
+}
